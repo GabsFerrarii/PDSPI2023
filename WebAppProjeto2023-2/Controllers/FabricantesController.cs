d09f0a7ec9670f05be1a93172fa4b4f4e0b6e703
Gabriel Ferreira Cavalcante <cavalcante.gabriel@academico.ifrn.edu.br>
2023-05-08 14:25:49 -0300

    Finalizei o crud com jquery e adicionei outros detalhes no código

--- WebAppProjeto2023-2/Controllers/FabricantesController.cs
+++ WebAppProjeto2023-2/Controllers/FabricantesController.cs
@@ -100,7 +100,10 @@ namespace WebAppProjeto2023_2.Controllers
             Fabricante fabricante = context.Fabricantes.Find(id);
             context.Fabricantes.Remove(fabricante);
             context.SaveChanges();
+            TempData["Message"] = "Fabricante " + fabricante.Nome.ToUpper() + " foi removido";
             return RedirectToAction("Index");
         }
+
+
     }
 }
