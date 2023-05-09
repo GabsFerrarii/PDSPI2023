f1162a2afd938988169ca8d12a3fb81b425a40a2
Gabriel Ferreira Cavalcante <cavalcante.gabriel@academico.ifrn.edu.br>
2023-05-09 15:54:48 -0300

    Começo do desenvolvimento da página inicial

---
+++ WebAppProjeto2023-2/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppProjeto2023_2.Models;
+
+namespace WebAppProjeto2023_2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private EFContext context = new EFContext();
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(context.Fabricantes.OrderBy(c => c.Nome));
+        }
+    }
+}
