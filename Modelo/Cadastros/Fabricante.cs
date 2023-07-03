e9115f0a82c5c012d32ce596e08e575589939690
Gabriel Ferreira Cavalcante <cavalcante.gabriel@academico.ifrn.edu.br>
2023-07-03 14:27:37 -0300

    Mudança da camada de Modelo para um projeto separado

--- Modelo/Cadastros/Fabricante.cs
+++ Modelo/Cadastros/Fabricante.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace WebAppProjeto2023_2.Models
+namespace Modelo.Cadastros
 {
     public class Fabricante
     {
--- Modelo/Cadastros/Produto.cs
+++ Modelo/Cadastros/Produto.cs
@@ -1,9 +1,10 @@
-﻿using System;
+﻿using Modelo.Tabelas;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace WebAppProjeto2023_2.Models
+namespace Modelo.Cadastros
 {
     public class Produto
     {
