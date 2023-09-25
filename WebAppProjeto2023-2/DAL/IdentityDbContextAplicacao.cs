38e9dffcfe10816ccf386f64982d63090562b33d
Gabriel Ferreira Cavalcante <cavalcante.gabriel@academico.ifrn.edu.br>
2023-09-25 14:28:33 -0300

    Criação do IdentityContext na aplicação

---
+++ WebAppProjeto2023-2/DAL/IdentityDbContextAplicacao.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
+using WebAppProjeto2023_2.Areas.Seguranca.Data;
+using System.Data.Entity;
+
+namespace WebAppProjeto2023_2.DAL
+{
+    public class IdentityDbContextAplicacao : IdentityDbContext<Usuario>
+    {
+        public IdentityDbContextAplicacao() : base("IdentityDb")
+        { }
+        static IdentityDbContextAplicacao()
+        {
+            Database.SetInitializer<IdentityDbContextAplicacao>(
+            new IdentityDbInit());
+        }
+        public static IdentityDbContextAplicacao Create()
+        {
+            return new IdentityDbContextAplicacao();
+        }
+        public class IdentityDbInit : DropCreateDatabaseIfModelChanges<IdentityDbContextAplicacao>
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<WebAppProjeto2023_2.Areas.Seguranca.Data.Usuario> Usuarios { get; set; }
+    }
+}
---
+++ WebAppProjeto2023-2/Infraestrutura/GerenciadorUsuario.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebAppProjeto2023_2.Areas.Seguranca.Data;
+using WebAppProjeto2023_2.DAL;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+
+namespace WebAppProjeto2023_2.Infraestrutura
+{
+    public class GerenciadorUsuario : UserManager<Usuario>
+    {
+        public GerenciadorUsuario(IUserStore<Usuario> store) : base(store)
+        { }
+        public static GerenciadorUsuario Create(
+        IdentityFactoryOptions<GerenciadorUsuario> options, IOwinContext context)
+        {
+            IdentityDbContextAplicacao db =
+            context.Get<IdentityDbContextAplicacao>();
+            GerenciadorUsuario manager = new GerenciadorUsuario(
+            new UserStore<Usuario>(db));
+            return manager;
+        }
+    }
+}
